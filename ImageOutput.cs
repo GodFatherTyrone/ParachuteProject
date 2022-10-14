a61426b462d258b648419823602014c279a10a32
NatalieTesam <nataliekay1023@gmail.com>
2022-10-13 21:21:12 -0600

    worked on ImageOutput.cs a bit

---
+++ ImageOutput.cs
@@ -0,0 +1,34 @@
+using System;
+// create parachute image and letter spaces
+
+namespace Placeholder
+{
+    // Responsibility of ImageOutput is to show and track the parachute changes
+    public class ImageOutput
+    {
+        // an array holding the different phases of the parachute man
+        private string[] _parachuteMan = {
+            "  ___  \n /___\\ \n \\   / \n  \\ /  \n   O   \n  /|\\  \n  / \\  \n^^^^^^^\n", 
+            " /___\\ \n \\   / \n  \\ /  \n   O   \n  /|\\  \n  / \\  \n^^^^^^^\n",
+            "  ___  \n \\   / \n  \\ /  \n   O   \n  /|\\  \n  / \\  \n^^^^^^^\n",
+            " \\   / \n  \\ /  \n   O   \n  /|\\  \n  / \\  \n^^^^^^^\n",
+            "  \\ /  \n   O   \n  /|\\  \n  / \\  \n^^^^^^^\n",
+            "   X   \n  /|\\  \n  / \\  \n^^^^^^^\n",
+
+        };
+        // Creates the image of the letter spaces and the parachute
+        public void DrawImage()
+        {
+
+        }
+
+        private void CutParachute()
+        {
+
+        }
+
+
+
+
+    }
+}
