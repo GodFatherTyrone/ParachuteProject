00a01153ba128958a19b8133336db8c2194dc5eb
Ty Budvarson <tygunnarbud@gmail.com>
2022-10-18 15:39:13 -0600

    Calls the Director

---
+++ program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ParachuteProject
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            Director director = new Director();
+            director.StartGame();
+            return 0;
+        }
+    }
+}
